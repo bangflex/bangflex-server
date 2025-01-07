4a9435c8829b45429d1753cd564646aa939cd255
gilhan <rlfgks97@gmail.com>
2025-01-07 22:19:10 +0900

    refactor: security configuration 리펙토링

--- springboot-monolithic/src/main/java/springbootmonolithic/security/config/SecurityConfiguration.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/security/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package springbootmonolithic.security;
+package springbootmonolithic.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
