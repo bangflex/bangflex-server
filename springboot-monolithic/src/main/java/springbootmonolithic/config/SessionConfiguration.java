2c231497932bb6ba9b0506c1421f744ce729beeb
gilhan <rlfgks97@gmail.com>
2025-01-07 22:30:03 +0900

    feat: session configuration 추가

---
+++ springboot-monolithic/src/main/java/springbootmonolithic/config/SessionConfiguration.java
@@ -0,0 +1,14 @@
+package springbootmonolithic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class SessionConfiguration {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
