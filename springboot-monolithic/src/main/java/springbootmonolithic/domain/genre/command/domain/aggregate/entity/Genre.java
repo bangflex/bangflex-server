70485318c2edf56a4f22a76d6cc4f66e514bf0eb
gilhan <rlfgks97@gmail.com>
2025-01-09 23:28:34 +0900

    feat: domain entity 추가

---
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/genre/command/domain/aggregate/entity/Genre.java
@@ -0,0 +1,4 @@
+package springbootmonolithic.domain.genre.command.domain.aggregate.entity;
+
+public class Genre {
+}
---
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/reaction/command/domain/aggregate/entity/Reaction.java
@@ -0,0 +1,4 @@
+package springbootmonolithic.domain.reaction.command.domain.aggregate.entity;
+
+public class Reaction {
+}
