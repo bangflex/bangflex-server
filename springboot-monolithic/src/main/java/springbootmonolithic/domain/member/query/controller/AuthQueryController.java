3f972cef023a689d0f513e72725b453ff9791128
gilhan <rlfgks97@gmail.com>
2025-01-15 17:47:38 +0900

    feat: email 검증 API 추가

---
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/controller/AuthQueryController.java
@@ -0,0 +1,40 @@
+package springbootmonolithic.domain.member.query.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springbootmonolithic.common.response.SuccessResponse;
+import springbootmonolithic.domain.member.query.service.AuthQueryService;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthQueryController {
+    private final AuthQueryService authQueryService;
+
+    @Autowired
+    public AuthQueryController(AuthQueryService authQueryService) {
+        this.authQueryService = authQueryService;
+    }
+
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "AuthQuery v1 good";
+    }
+
+    @GetMapping("email/check")
+    public ResponseEntity<SuccessResponse<Void>> emailCheck(@RequestParam(name = "email") String email) {
+        authQueryService.validateEmail(email);
+
+        return ResponseEntity.ok(
+                new SuccessResponse<>(
+                        "사용 가능한 이메일입니다.", null, LocalDateTime.now()
+                )
+        );
+    }
+}
