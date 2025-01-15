5e2dca39942142b07c75017efc9d98c2dd3c4efa
gilhan <rlfgks97@gmail.com>
2025-01-15 18:30:08 +0900

    feat: logger & 헬퍼메서드 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/common/response/ErrorResponse.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/common/response/ErrorResponse.java
@@ -1,13 +1,20 @@
 package springbootmonolithic.common.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@Builder
+@Getter
 @ToString
 public class ErrorResponse {
-    private String message;
-    private LocalDateTime timestamp;
+    private final String message;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime timestamp;
 }
