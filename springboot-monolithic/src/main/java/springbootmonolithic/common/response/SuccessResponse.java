56e854f0ed0447b71141bac32e9901fd6e484ef0
gilhan <rlfgks97@gmail.com>
2025-01-15 17:45:05 +0900

    feat: localDateTime formatter 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/common/response/SuccessResponse.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/common/response/SuccessResponse.java
@@ -1,14 +1,19 @@
 package springbootmonolithic.common.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@Getter
 @ToString
 public class SuccessResponse<T> {
-    private final String message;
-    private final T result;
+    private String message;
+    private T result;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime timestamp;
 }
