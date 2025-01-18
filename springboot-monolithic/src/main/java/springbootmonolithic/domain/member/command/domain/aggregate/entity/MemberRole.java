7ee7270a00ed87162ba8fed0c88574c1fef718b0
gilhan <rlfgks97@gmail.com>
2025-01-19 05:37:22 +0900

    feat: MemberRole 기본생성자 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/domain/member/command/domain/aggregate/entity/MemberRole.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/member/command/domain/aggregate/entity/MemberRole.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import springbootmonolithic.domain.member.command.domain.aggregate.entity.compositekey.MemberRolePK;
 
 @Entity
@@ -13,6 +14,7 @@ import springbootmonolithic.domain.member.command.domain.aggregate.entity.compos
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberRole {
     @Id
     @Column(name = "member_code")
