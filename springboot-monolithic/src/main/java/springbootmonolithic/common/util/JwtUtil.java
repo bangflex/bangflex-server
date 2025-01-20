f1ad9dcff0fcd2af475fc7518cbe949774b87a1e
gilhan <rlfgks97@gmail.com>
2025-01-20 23:32:57 +0900

    feat: jwt token utilzation 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/common/util/JwtUtil.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/common/util/JwtUtil.java
@@ -1,18 +1,23 @@
 package springbootmonolithic.common.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import springbootmonolithic.domain.member.query.service.AuthQueryService;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -22,15 +27,18 @@ import java.util.stream.Collectors;
 public class JwtUtil {
     private final Key key;
     private final Environment environment;
+    private final AuthQueryService authService;
 
     @Autowired
     public JwtUtil(
             @Value("${token.secret}") String secretKey,
-            Environment environment
+            Environment environment,
+            AuthQueryService authService
     ) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.environment = environment;
+        this.authService = authService;
     }
 
     public String generateAccessToken(Authentication authentication) {
@@ -64,5 +72,59 @@ public class JwtUtil {
                 )
                 .signWith(key)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key).build()
+                    .parseClaimsJws(token);
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.error("Invalid token {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("Expired token {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported token {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("Empty token {}", e.getMessage());
+        }
+        return true;
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+
+        Collection<? extends GrantedAuthority> authorities = null;
+
+        if (claims.get("authorities") != null) {
+            authorities = Arrays.stream(
+                            claims.get("authorities").toString()
+                                    .replace("[", "")
+                                    .replace("]", "")
+                                    .split(", ")
+                    )
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        } else {
+            log.error("No authorities found in token");
+            throw new IllegalArgumentException("No authorities found in token");
+        }
+        UserDetails savedUser = authService.loadUserByUsername(getLoginId(token));
+        return new UsernamePasswordAuthenticationToken(
+                savedUser,
+                savedUser.getPassword(),
+                authorities
+        );
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getLoginId(String token)  {
+        return parseClaims(token).getSubject();
     }
 }
