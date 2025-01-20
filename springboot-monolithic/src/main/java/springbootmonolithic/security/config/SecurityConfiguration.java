4b2f01db85fa8158e4c7b80ee5d009de60ec7ead
gilhan <rlfgks97@gmail.com>
2025-01-20 16:29:20 +0900

    feat: DaoAuthenticationFilter 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/security/config/SecurityConfiguration.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/security/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package springbootmonolithic.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,28 +9,43 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import springbootmonolithic.security.filter.DaoAuthenticationFilter;
+import springbootmonolithic.security.provider.ProviderManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final ProviderManager providerManager;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public SecurityConfiguration(
+            ProviderManager providerManager,
+            ObjectMapper objectMapper
+    ) {
+        this.providerManager = providerManager;
+        this.objectMapper = objectMapper;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-
+        http.csrf(AbstractHttpConfigurer::disable);  // csrf 비활성화
         http.formLogin(AbstractHttpConfigurer::disable);
-
-        http.sessionManagement(sessionManagement ->
+        http.sessionManagement(sessionManagement ->  // 세션 비활성화
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(authorize ->
                 authorize
-                        .requestMatchers("/**").permitAll()
+//                        .requestMatchers("/**").permitAll()
 //                        .requestMatchers("/api/v1/check/**").permitAll()
-//                        .requestMatchers("/api/v1/auth/**").permitAll()
-//                        .requestMatchers("/swagger-ui/**").permitAll()
-//                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilter(new DaoAuthenticationFilter(providerManager, objectMapper));
+
         return http.build();
     }
 }
---
+++ springboot-monolithic/src/main/java/springbootmonolithic/security/filter/DaoAuthenticationFilter.java
@@ -0,0 +1,72 @@
+package springbootmonolithic.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springbootmonolithic.common.response.SuccessResponse;
+import springbootmonolithic.security.dto.LoginRequestDTO;
+import springbootmonolithic.security.provider.ProviderManager;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Slf4j
+public class DaoAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final ProviderManager providerManager;
+    private final ObjectMapper objectMapper;
+
+    public DaoAuthenticationFilter(
+            ProviderManager providerManager,
+            ObjectMapper objectMapper
+    ) {
+        this.providerManager = providerManager;
+        this.objectMapper = objectMapper;
+        this.setFilterProcessesUrl("/api/v1/auth/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestDTO loginRequestDTO = objectMapper.readValue(request.getInputStream(), LoginRequestDTO.class);
+            return providerManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequestDTO.getLoginId(), loginRequestDTO.getPassword(), new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult
+    ) throws IOException {
+
+        // 응답 헤더 및 상태 코드 설정
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // ResponseMessage 객체를 JSON으로 변환 후 응답에 작성
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        SuccessResponse.<Boolean>builder()
+                                .message("로그인 성공")
+                                .result(true)
+                                .timestamp(LocalDateTime.now())
+                                .build()
+                )
+        );
+    }
+}
