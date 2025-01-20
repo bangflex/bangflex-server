06a2bb39eb08725b7fe459a3bfd08719bfe63ad8
gilhan <rlfgks97@gmail.com>
2025-01-20 16:27:56 +0900

    feat: login을 위한 서비스 메서드 추가 - loadUserByUsername

--- springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/service/AuthQueryServiceImpl.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/service/AuthQueryServiceImpl.java
@@ -1,11 +1,23 @@
 package springbootmonolithic.domain.member.query.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import springbootmonolithic.domain.member.query.dto.MemberDTO;
+import springbootmonolithic.domain.member.query.dto.RoleDTO;
 import springbootmonolithic.domain.member.query.mapper.AuthMapper;
 import springbootmonolithic.exception.EmailDuplicatedException;
+import springbootmonolithic.security.domain.CustomUser;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
 @Service
 public class AuthQueryServiceImpl implements AuthQueryService {
     private final AuthMapper authMapper;
@@ -26,5 +38,28 @@ public class AuthQueryServiceImpl implements AuthQueryService {
     @Override
     public boolean checkEmailDuplicated(String email) {
         return authMapper.isExistsByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        MemberDTO loginMember = authMapper.selectMemberByEmailWithAuthorities(email);
+
+        if (loginMember == null) {
+            throw new UsernameNotFoundException("email이 존재하지 않습니다.");
+        }
+        log.debug("loginMember: {}", loginMember);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (RoleDTO roleDTO : loginMember.getRoleDTO()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(roleDTO.getRole()));
+        }
+
+        return new CustomUser(
+                loginMember.getCode(),
+                loginMember.getEmail(),
+                loginMember.getPassword(),
+                loginMember.getNickname(),
+                grantedAuthorities
+        );
     }
 }
