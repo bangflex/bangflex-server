8506c9447d01c2001bca520c9ac264f1e6d93f37
gilhan <rlfgks97@gmail.com>
2025-01-21 14:14:05 +0900

    test: 테스트코드를 위한 loginDTO @Allargs 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/security/dto/LoginRequestDTO.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/security/dto/LoginRequestDTO.java
@@ -1,13 +1,11 @@
 package springbootmonolithic.security.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class LoginRequestDTO {
     private String loginId;
