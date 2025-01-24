de2cae1cb67c9d1a1943cc9b0e12d091594729e7
gilhan <rlfgks97@gmail.com>
2025-01-25 03:38:59 +0900

    feat: 이메일로 회원 정보 조회 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/service/MemberQueryServiceImpl.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/service/MemberQueryServiceImpl.java
@@ -1,12 +1,16 @@
 package springbootmonolithic.domain.member.query.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import springbootmonolithic.domain.member.query.dto.MemberInformationDTO;
 import springbootmonolithic.domain.member.query.mapper.MemberMapper;
 import springbootmonolithic.exception.EmailDuplicatedException;
+import springbootmonolithic.exception.MemberNotFoundException;
 import springbootmonolithic.exception.NicknameDuplicatedException;
 
+@Slf4j
 @Service
 public class MemberQueryServiceImpl implements MemberQueryService {
     private final MemberMapper memberMapper;
@@ -34,5 +38,21 @@ public class MemberQueryServiceImpl implements MemberQueryService {
     @Transactional
     public void checkNicknameDuplicated(String nickname) {
         if (memberMapper.isExistsByNickname(nickname)) throw new NicknameDuplicatedException("이미 사용중인 닉네임입니다.");
+    }
+
+    /**
+     * 이메일로 회원 정보 조회
+     *
+     * @param email 이메일
+     * @return 회원 정보
+     */
+    @Override
+    @Transactional
+    public MemberInformationDTO getMemberInformationByEmail(String email) {
+        MemberInformationDTO memberInformationDTO = memberMapper.selectMemberInformationByEmail(email);
+        if (memberInformationDTO == null) {
+            throw new MemberNotFoundException("해당 이메일로 가입된 회원이 없습니다.");
+        }
+        return memberInformationDTO;
     }
 }
