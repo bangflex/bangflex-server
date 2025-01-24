172b6dbd2b7963d1dafd6f0cf5e04d9d630ec472
gilhan <rlfgks97@gmail.com>
2025-01-25 03:38:38 +0900

    feat: 회원 정보 조회 매퍼 추가

--- springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/mapper/MemberMapper.java
+++ springboot-monolithic/src/main/java/springbootmonolithic/domain/member/query/mapper/MemberMapper.java
@@ -1,9 +1,11 @@
 package springbootmonolithic.domain.member.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import springbootmonolithic.domain.member.query.dto.MemberInformationDTO;
 
 @Mapper
 public interface MemberMapper {
     boolean isExistsByNickname(String nickname);
     boolean isExistsByEmail(String email);
+    MemberInformationDTO selectMemberInformationByEmail(String email);
 }
